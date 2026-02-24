fc521f599d7054ed5e23746ed9d30579dee9e465
Nursalam <35249316+asephermann@users.noreply.github.com>
2026-02-25 06:04:59 +0700

    Add explanation of Hubungan Tanpa Status (HTS)

---
+++ h.ts
@@ -0,0 +1,13 @@
+HTS adalah singkatan dari Hubungan Tanpa Status. Istilah bahasa gaul ini digunakan untuk menggambarkan situasi di mana dua orang menjalin kedekatan romantis atau emosional, namun sepakat (atau secara tidak sadar) untuk tidak memberikan label resmi seperti "pacaran". 
+Dalam bahasa Inggris, fenomena ini sering disebut sebagai situationship. 
+
+Karakteristik Utama HTS
+Tanpa Komitmen: Tidak ada janji setia atau kewajiban jangka panjang di antara kedua pihak.
+Komunikasi Tidak Stabil: Intensitas komunikasi bisa berubah-ubah secara tiba-tiba tanpa alasan yang jelas.
+Privasi Terjaga: Biasanya tidak diperkenalkan kepada orang tua atau lingkaran pertemanan sebagai pasangan resmi.
+Tidak Ada Eksklusivitas: Karena tidak ada status, masing-masing pihak seringkali merasa bebas untuk dekat dengan orang lain. 
+
+Risiko Menjalani HTS
+Ketidakpastian Emosional: Sering menimbulkan kebingungan karena tidak adanya kejelasan masa depan hubungan.
+Pembatasan Perasaan: Munculnya rasa cemburu seringkali dianggap tidak valid karena "bukan siapa-siapa".
+Rentan Ditinggalkan: Salah satu pihak bisa pergi kapan saja tanpa merasa perlu memberikan penjelasan atau pertanggungjawaban. 
